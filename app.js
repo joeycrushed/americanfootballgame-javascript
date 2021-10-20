7caa87c291c740ea5399d9c4d9d9c07d18151f63
Joe Smith <joeycrushed@gmail.com>
2021-10-20 18:43:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+
+const form = document.querySelector('#form')
+const pickCoin = document.querySelector('#cointoss')
+const coinToss = document.getElementById('flipcoin')
+const playButton = document.querySelector(".playbutton")
+
+// Team Objects
+
+let yourTeam = {
+    position: 0,
+    hasBall: false,
+    coin: ""
+}   
+
+let otherTeam = {
+    position:  0,
+    hasBall: false,
+    coin: ""
+}
+
+// Pick coin
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    yourTeam.coin = pickCoin.value
+    document.querySelector('#yourpick').innerHTML = `You have picked ${pickCoin.value}`
+    console.log(yourTeam)
+})
+
+// Coin Flip
+
+
+coinToss.addEventListener('click', flipCoin)
+
+function flipCoin(event) {
+    let num = Math.random();
+    let coin = ""
+    results = document.getElementById("heads_tails")
+     if(num < 0.5) {
+        coin = "Heads"
+        } else {
+        coin = "Tails"
+     }
+     if(coin == yourTeam.coin) {
+         console.log("you win")
+         results.innerHTML = `It was ${coin}, You get the ball`
+         yourTeam.hasBall = true
+     } else if (coin != yourTeam.coin) {
+         results.innerHTML = `It was ${coin}, The other team get the ball`
+         otherTeam.hasBall = true
+     }
+     playButton.classList.remove('hidden')
+}
+
+// Kick off
+
+playButton.addEventListener('click', (e) => {
+    form.classList.add('hidden')
+    playButton.classList.add('hidden')
+    results.classList.add('hidden')
+})
